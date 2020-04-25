7e9175acd44e3cb72d567414b6a4494f56e37c36
Saif Ahmed <34284663+saiftynet@users.noreply.github.com>
2020-04-25 20:05:34 +0100

    Version 0.02
    Asciiloscope now allows sample rate change, magifiy and reduce vertical scale

--- src/asciilloscope.pl
+++ src/asciilloscope.pl
@@ -1,4 +1,7 @@
-#perl 5.22.1 
+#!/usr/env perl
+# ASCIILOSCOPE
+# A Terminanl based real-time analog data visualistaion tool.
+
 use strict;use warnings;
 
 use utf8;
@@ -10,8 +13,10 @@ use Term::ReadKey;
 my $key;
 my $OrdKey;
 
-my $VERSION=0.01;
+my $VERSION=0.02;
 
+# display parameters stored in hash for future conversion into an
+# object orientated 
 my %display=(                  # display parameters
    borderStyle=>"double",      # border style
    height    =>14,             # vertical characters
@@ -19,14 +24,38 @@ my %display=(                  # display parameters
    row       =>2,              # vertical position (from top left)
    column    =>10,             # horizontal position
    sampleRate=>100,            # number of samples per second
-   symbol=>"*",);              # plot symbol
+   symbol    =>"*",);              # plot symbol
 
 my %actions=(                  # for keyboard driven actions
+   113=>{
+	   note=>"q = exit",
+	   proc=>sub{ exit;},
+   },
+   97=>{
+	   note=>"a = Auto levels",
+	   proc=>sub{autoLevels()},
+   },
+   67=>{
+	   note=>"🠞 = Speed up",
+	   proc=>sub{$display{sampleRate}+=10;$OrdKey=0;},
+   },
+   68=>{
+	   note=>"🠜 = Slow down",
+	   proc=>sub{$display{sampleRate}=$display{sampleRate}>10?$display{sampleRate}-10:10;$OrdKey=0;},
+   },
+   65=>{
+	   note=>"🠉 = Magnify",
+	   proc=>sub{$display{yMult}*=1.1;$OrdKey=0;},
+   },
+   66=>{
+	   note=>"🠋 = Reduce",
+	   proc=>sub{$display{yMult}*=0.9;$OrdKey=0;},
+   },
    
 
 );
 
-# example initial dataset...a sine wave   
+# example initial dataset...a sine wave preloaded to allow scaling
 my @list=();                                 
 push @list,sin (3.14*$_/20) for (0..55); 
 my $next=@list;
@@ -37,8 +66,6 @@ autoLevels();      # auto adjust the scaling based on initial sample
 startScope();      # the loop that updates the scope's display
 
 print "\033[18;0H";#jump to 18,0 
-
-
 
 # draws the frame and other features outside the 
 sub initialScreen{           
@@ -54,14 +81,16 @@ sub initialScreen{
 	unshift @plotArea,$border{tl}.($border{t}x$display{width}).$border{tr};
     push    @plotArea,$border{bl}.($border{b}x$display{width}).$border{br};
     printAt($display{row},$display{column},@plotArea);
-    
-    printAt($display{row}+$display{height}+3,$display{column}-7,
-    '   _    ___   ___   _______  _    ___   ___   __   ___   ___  ___',
-    '  /_\  / __| / __| |_ _|_ _|| |  / _ \ / __| / _| / _ \ |  _\| __|',
-    ' / _ \ \__ \ | (__  | | | | | |_| (_) |\__ \| (_ | (_) || |_/| _| ',
-    '/_/ \_\|___/ \___| |___|___||___|\___/ |___/ \__| \___/ |_|  |___|',
-    );
 
+    printAt( 3,$display{width}+$display{column}+3,
+        map{$actions{$_}{note} } sort { $a <=> $b } keys %actions);
+    
+    printAt($display{row}+$display{height}+3,$display{column}-7<0?0:$display{column}-7,
+    '   _    ___   __  _______  _    ___   ___   __   ___   ___  ___',
+    '  /_\  / __| / _||_ _|_ _|| |  / _ \ / __| / _| / _ \ |  _\| __|',
+    ' / _ \ \__ \| (_  | | | | | |_| (_) |\__ \| (_ | (_) || |_/| _| ',
+    '/_/ \_\|___/ \__||___|___||___|\___/ |___/ \__| \___/ |_|  |___|'." v$VERSION",
+    );
 }
 
 # uses the data in the @list to autscale the waveform for display
@@ -72,7 +101,7 @@ sub autoLevels{
     $min=$y if  $y<$min;
   } 
   $display{yMult}=($display{height}-2)/($max-$min);
-  $display{yOffset}=-$min*$display{yMult};
+  $display{yOffset}=-$min*$display{yMult}+1;
   $display{xMult}=$display{width}/(scalar @list);
 	
 }
@@ -89,21 +118,23 @@ sub startScope{
 	  $key = ReadKey(-1);                # non-blocking read of keyboard
 	  $OrdKey = ord($key) if $key;       # read key
 	  if ($OrdKey){
+		printAt( 1,$display{width}+$display{column}+2,"Key pressed = $OrdKey  ");
 	    last if $OrdKey ==113 ;          # q is the quit key
 	                                     # other keys actions are stored in %actions
-	    $actions{$OrdKey}->() if defined $actions{$OrdKey};
+	    $actions{$OrdKey}{proc}->() if defined $actions{$OrdKey};
 	  }
 	}  
   };
 
 # generates plots from the list by scaling to fit into display area      
 sub scatterPlot{
-  my @plots=map { ([int( $_*$display{xMult}) , int($display{yMult}*$list[$_] +$display{yOffset})]) } (0..$#list);
+  my @plots=map { [int( $_*$display{xMult}) ,
+	  bound (int($display{yMult}*$list[$_] +$display{yOffset}),0,$display{height}-1)] } (0..$#list);
   my @rows=(" "x$display{width})x$display{height};
   foreach (@plots){
     substr ($rows[$$_[1]], $$_[0],1,$display{symbol});
   }
-  printAt($display{row}+1,$display{column}+1,reverse @rows);
+  printAt($display{row}+2,$display{column}+1,reverse @rows);
 }
 # routine that prints multiline strings at specific points on the terminal window
 sub printAt{
@@ -113,6 +144,13 @@ sub printAt{
 	print $blit;
 }	
 
+# sets the boundaries for a number assignment 
+sub bound{  
+	my ($number,$min,$max)=@_;
+	return $max if $number>$max;
+	return $min if $number<$min;
+	return $number;	
+}
 
 
 
