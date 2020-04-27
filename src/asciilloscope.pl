3a6907ad9b1829eacce9f350952da3d82cdb6e2a
Saif Ahmed <34284663+saiftynet@users.noreply.github.com>
2020-04-27 21:43:22 +0100

    Version 0.05
    Start colourisation, allow menu and logo to be disabled

--- src/asciilloscope.pl
+++ src/asciilloscope.pl
@@ -10,14 +10,17 @@ use Time::HiRes ("sleep");      # allow fractional sleeps
 use Term::ReadKey;              # allow reading from keyboard
 my $key;                      
 
-my $VERSION=0.04;
+my $VERSION=0.05;
 
 # display parameters stored in hash for future conversion into an
-# object orientated 
+# object orientated module 
 
-my @list;
+my @list;   # the data window
 
 my %display=(                  # display parameters
+   showLogo   =>1,             # show logo or not
+   showMenu   =>1,             # show menu or not
+   enableColours=>1,           # enable colours
    borderStyle=>"double",      # border style
    height    =>14,             # vertical characters
    width     =>50,             # horizontal characters
@@ -26,7 +29,8 @@ my %display=(                  # display parameters
    sampleRate=>100,            # number of samples per second
    dataWindow=>55,             # number of samples in one window
    dataStore =>110,
-   symbol    =>"*",);          # plot symbol
+   symbol    =>"*",            # plot symbol
+   );        
 
 my %actions=(                  # for keyboard driven actions
    113=>{ # q exits
@@ -60,8 +64,7 @@ my %actions=(                  # for keyboard driven actions
    66 =>{ # shift display down by 1
 	   note=>"🠋 = Shift down",
 	   proc=>sub{$display{yOffset}-=1;},
-   },
-   
+   },   
    42 =>{ # increase samples per full width
 	   note=>"* = Inc Window",
 	   proc=>sub{unshift @list,($list[0]) x 10;
@@ -69,7 +72,7 @@ my %actions=(                  # for keyboard driven actions
    },
    47 =>{ # decrease samples per full width
 	   note=>"/ = Dec Window",
-	   proc=>sub{@list=@list[9..$#list];
+	   proc=>sub{@list=@list[9..$#list] if @list>15;
 		         $display{xMult}=$display{width}/(scalar @list);},
    },
    43 =>{ # increase multiplier by 10%
@@ -80,8 +83,6 @@ my %actions=(                  # for keyboard driven actions
 	   note=>"- = Reduce",
 	   proc=>sub{$display{yMult}*=0.9;},
    },
-   
-   
 );
 
 # example initial dataset...a sine wave preloaded to allow scaling -1 to 1
@@ -105,19 +106,21 @@ sub initialScreen{
 	foreach (0..$display{height}){
 		$plotArea[$_]=$border{l}.(" "x$display{width}).$border{r};
 	}
-	unshift @plotArea,$border{tl}.($border{t}x$display{width}).$border{tr};
-    push    @plotArea,$border{bl}.($border{b}x$display{width}).$border{br};
-    printAt($display{row},$display{column},@plotArea);
+	unshift @plotArea,colour("blue","bold").$border{tl}.($border{t}x$display{width}).$border{tr};
+    push    @plotArea,$border{bl}.($border{b}x$display{width}).$border{br}.colour("reset");
+    printAt($display{row},$display{column},@plotArea),;
 
     printAt( 3,$display{width}+$display{column}+3,
-        map{$actions{$_}{note} } sort { $a <=> $b } keys %actions);
+        map{$actions{$_}{note} } sort { $a <=> $b } keys %actions) if $display{showMenu};;
     
     printAt($display{row}+$display{height}+3,$display{column}-7<0?0:$display{column}-7,
+    colour("yellow","bold").
     '   _    ___   __  _______  _    ___   ___   __   ___   ___  ___',
     '  /_\  / __| / _||_ _|_ _|| |  / _ \ / __| / _| / _ \ |  _\| __|',
     ' / _ \ \__ \| (_  | | | | | |_| (_) |\__ \| (_ | (_) || |_/| _| ',
-    '/_/ \_\|___/ \__||___|___||___|\___/ |___/ \__| \___/ |_|  |___|'." v$VERSION",
-    );
+    '/_/ \_\|___/ \__||___|___||___|\___/ |___/ \__| \___/ |_|  |___|'." v$VERSION".
+    colour("reset") 
+    ) if $display{showLogo};
 };
 
 # uses the data in the @list to autscale the waveform for display
@@ -139,11 +142,11 @@ sub startScope{
     unless ($display{pause}){
       shift @list;
 	  push @list, sin (3.14*$next++/20); # the next data capture pushed into list
-	  $next=0 if $next>200;              # limit the size of the trace
+	  $next=0 if $next>200;              # limit the size of $next...
     }
     scatterPlot();                     # draw the trace
 	sleep 1/$display{sampleRate};      # pause
-	$key = ReadKey(-1);                #
+	$key = ReadKey(-1);                # -1 means non-blocking read
 	if ($key){
 	  my $OrdKey = ord($key);
 	  printAt( 1,$display{width}+$display{column}+2,"Key pressed = $OrdKey  ");
@@ -151,6 +154,7 @@ sub startScope{
 	  $actions{$OrdKey}{proc}->() if defined $actions{$OrdKey};
 	}
   }  
+  ReadMode 'normal';
 };
 
 # generates plots from the list by scaling to fit into display area      
@@ -181,5 +185,10 @@ sub bound{
 	return $number;	
 }
 
-
-
+sub colour{
+	return "" unless $display{enableColours};
+	my @formats=map {lc $_} @_;
+	my %colours=(black=>30,red=>31,green=>32,yellow=>33,blue=>34,magenta=>35,cyan=>36,white=>37,reset=>0,
+	             bold=>1, italic=>3, underline=>4, strikethrough=>9,);
+	return join "",map {"\033[$colours{$_}m"}@formats;
+}
